683a6767d1c4068222d5d4f85604668ac713d3d3
eric-tong <erictong.designs@gmail.com>
2020-03-27 15:55:11 +0800

    [Styles] Add main styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import "./styles/normalize.scss";
+import "./styles/main-styles.scss";
+
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <h1>Free Covid Services</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </>
   );
 }
 
