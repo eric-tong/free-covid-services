90f96182a9d1dd60adbeaabb27949ce86aeeb7d7
eric-tong <erictong.designs@gmail.com>
2020-03-27 17:53:02 +0800

    [Data] Add data folders

---
+++ src/data/arts-and-culture/ArtsAndCulture.ts
@@ -0,0 +1,5 @@
+import Theatre from "./Theatre";
+
+const ArtsAndCulture: Map<string, Service[]> = new Map([["Theatre", Theatre]]);
+
+export default ArtsAndCulture;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare type Service = {
+  name: string;
+  description: string;
+  url: string;
+  startDate?: string;
+};
