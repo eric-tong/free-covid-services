dc0e1e2a7088cb1d73bc8293f380c0e652f57853
eric-tong <erictong.designs@gmail.com>
2020-03-27 17:35:57 +0800

    [Header] Add description section

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,21 +4,34 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      <h1>
-        <span role="img" aria-label="Orange heart emoji">
-          🧡
-        </span>{" "}
-        FCS
-      </h1>
-      <p>
-        An{" "}
+    <>
+      <header>
+        <h1>
+          <span role="img" aria-label="Orange heart emoji">
+            🧡
+          </span>{" "}
+          FCS
+        </h1>
+        <p>
+          An{" "}
+          <a href="https://github.com/eric-tong/free-covid-services">
+            open-source
+          </a>{" "}
+          directory of companies offering free and discounted services during
+          the COVID-19 pandemic.
+        </p>
+      </header>
+      <h2 id="description">
+        From free theatre performances to software discounts, we have curated a
+        list of free or discounted services that might be helpful to you or your
+        business during these difficult times. If you are a small business
+        offering a helpful service and would like to be featured here, please{" "}
+        <a href="/reach-out">reach out</a> or send{" "}
         <a href="https://github.com/eric-tong/free-covid-services">
-          open-source
-        </a>{" "}
-        directory of companies offering free and discounted services during the
-        COVID-19 pandemic.
-      </p>
-    </header>
+          a pull request
+        </a>
+        .
+      </h2>
+    </>
   );
 }
