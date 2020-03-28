7119fe4d8fa9da619a3e6262871cc1959dac9bce
eric-tong <erictong.designs@gmail.com>
2020-03-28 12:21:38 +0800

    [Data] Prevent refetching every rerender

--- src/data/useServices.ts
+++ src/data/useServices.ts
@@ -1,14 +1,19 @@
+import { useEffect, useState } from "react";
+
 import { servicesTable } from "./database";
-import { useState } from "react";
 
 export default function useServices() {
   const [services, setServices] = useState<Service[]>([]);
-  servicesTable
-    .select({
-      view: "Grid view"
-    })
-    .firstPage()
-    .then(records => records.map(record => record.fields))
-    .then(setServices);
+
+  useEffect(() => {
+    servicesTable
+      .select({
+        view: "Grid view"
+      })
+      .firstPage()
+      .then(records => records.map(record => record.fields))
+      .then(setServices);
+  }, []);
+
   return services;
 }
