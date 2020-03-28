2f8a3452b269b23fc6d9c743806855b68e95a2e3
eric-tong <erictong.designs@gmail.com>
2020-03-28 16:54:53 +0800

    [Performance] Add react-snap

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import App from "./layout/App";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Missing root element");
+
+if (rootElement.hasChildNodes()) {
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
