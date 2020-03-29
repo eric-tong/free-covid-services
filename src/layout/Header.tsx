32addb7f5740fb3c6c4a93da9d1c1f0135321f8a
eric-tong <erictong.designs@gmail.com>
2020-03-29 15:12:10 +0800

    [Header] Change copy

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
       <h2 id="description">
         From free theatre performances to software discounts, we have curated a
         list of free or discounted services that might be helpful to you or your
-        business during these difficult times. If you are a small business
+        business during these difficult times. If you are an organisation
         offering a helpful service and would like to be featured here, please
         fill in <a href="https://airtable.com/shrXH3xUFypxOS6yb">this form</a>.
       </h2>
