799b6e1e8c7b749bc4ce4ad8d29ea4912645267a
eric-tong <erictong.designs@gmail.com>
2020-03-29 16:45:54 +0800

    [Logging] Add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import * as Sentry from "@sentry/browser";
+
 import App from "./layout/App";
 import React from "react";
 import ReactDOM from "react-dom";
+
+Sentry.init({
+  dsn: "https://47d29ede518243b0bd4303fa287107ac@sentry.io/5178914"
+});
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Missing root element");
